418d801a588f70678711a7b8e32def6e57280fcd
NamcheolLee <nclee@redwood.snu.ac.kr>
2025-09-20 21:00:28 +0900

    deleted unnecessary prefix

--- src/instrumentation_harness.cpp
+++ src/instrumentation_harness.cpp
@@ -22,12 +22,6 @@
 #include "util.hpp"
 #include "instrumentation_harness_utils.hpp"
 
-/* ================= Variable Naming Convention =================
-* Variables that start with a prefix _litert_ 
-* are objects or pointers directly related to inference via LiteRT 
-* This includes the model, interpreter, delegates, and tensors
-* ============================================================ */
-
 /* ============ Function Naming Convention of LiteRT ============
  * Public C++ class methods: UpperCamelCase (e.g., BuildFromFile)
  * Internal helpers: snake_case (e.g., typed_input_tensor)
@@ -55,8 +49,8 @@ int main(int argc, char *argv[])
     }
 
     /* Load .tflite model */
-    std::unique_ptr<tflite::FlatBufferModel> _litert_model = tflite::FlatBufferModel::BuildFromFile(model_path.c_str());
-    if (!_litert_model)
+    std::unique_ptr<tflite::FlatBufferModel> model = tflite::FlatBufferModel::BuildFromFile(model_path.c_str());
+    if (!model)
     {
         std::cerr << "Failed to load model" << std::endl;
         return 1;
@@ -64,58 +58,58 @@ int main(int argc, char *argv[])
     instrumentation::inspect_model_loading();
 
     /* Build interpreter */
-    tflite::ops::builtin::BuiltinOpResolver _litert_resolver;
-    tflite::InterpreterBuilder _litert_builder(*_litert_model, _litert_resolver);
-    std::unique_ptr<tflite::Interpreter> _litert_interpreter;
-    _litert_builder(&_litert_interpreter);
-    if (!_litert_interpreter)
+    tflite::ops::builtin::BuiltinOpResolver resolver;
+    tflite::InterpreterBuilder builder(*model, resolver);
+    std::unique_ptr<tflite::Interpreter> interpreter;
+    builder(&interpreter);
+    if (!interpreter)
     {
         std::cerr << "Failed to Initialize Interpreter" << std::endl;
         return 1;
     }
-    instrumentation::inspect_interpreter_instantiation(_litert_model.get(), _litert_resolver, _litert_interpreter.get());
-    instrumentation::inspect_interpreter(_litert_interpreter.get());
+    instrumentation::inspect_interpreter_instantiation(model.get(), resolver, interpreter.get());
+    instrumentation::inspect_interpreter(interpreter.get());
 
     /* Apply either XNNPACK delegate or GPU delegate */
-    TfLiteDelegate* _litert_xnn_delegate = TfLiteXNNPackDelegateCreate(nullptr);
-    TfLiteDelegate* _litert_gpu_delegate = TfLiteGpuDelegateV2Create(nullptr);
+    TfLiteDelegate* xnn_delegate = TfLiteXNNPackDelegateCreate(nullptr);
+    TfLiteDelegate* gpu_delegate = TfLiteGpuDelegateV2Create(nullptr);
     bool delegate_applied = false;
     if(gpu_usage) {
-        if (_litert_interpreter->ModifyGraphWithDelegate(_litert_gpu_delegate) == kTfLiteOk)
+        if (interpreter->ModifyGraphWithDelegate(gpu_delegate) == kTfLiteOk)
         {
             delegate_applied = true;
             // Delete unused delegate
-            if(_litert_xnn_delegate) TfLiteXNNPackDelegateDelete(_litert_xnn_delegate);
+            if(xnn_delegate) TfLiteXNNPackDelegateDelete(xnn_delegate);
         } else {
             std::cerr << "Failed to Apply GPU Delegate" << std::endl;
         }
     } else {
-        if (_litert_interpreter->ModifyGraphWithDelegate(_litert_xnn_delegate) == kTfLiteOk)
+        if (interpreter->ModifyGraphWithDelegate(xnn_delegate) == kTfLiteOk)
         {
             delegate_applied = true;
             // Delete unused delegate
-            if(_litert_gpu_delegate) TfLiteGpuDelegateV2Delete(_litert_gpu_delegate);
+            if(gpu_delegate) TfLiteGpuDelegateV2Delete(gpu_delegate);
         } else {
             std::cerr << "Failed to Apply XNNPACK Delegate" << std::endl;
         }
     }
-    instrumentation::inspect_interpreter_with_delegate(_litert_interpreter.get());
+    instrumentation::inspect_interpreter_with_delegate(interpreter.get());
 
     /* Allocate Tensor */
-    instrumentation::inspect_tensors(_litert_interpreter.get(), "Before Allocate Tensors");
-    if (_litert_interpreter->AllocateTensors() != kTfLiteOk)
+    instrumentation::inspect_tensors(interpreter.get(), "Before Allocate Tensors");
+    if (interpreter->AllocateTensors() != kTfLiteOk)
     {
         std::cerr << "Failed to Allocate Tensors" << std::endl;
         return 1;
     }
-    instrumentation::inspect_tensors(_litert_interpreter.get(), "After Allocate Tensors");
+    instrumentation::inspect_tensors(interpreter.get(), "After Allocate Tensors");
 
     /* Preprocessing */
     // We skip the actual preprocessing step here, as it is not the focus of this code
 
     /* Inference */
     // We skip the actual inference step here, as it is not the focus of this code
-    instrumentation::inspect_inference(_litert_interpreter.get());
+    instrumentation::inspect_inference(interpreter.get());
 
     /* PostProcessing */
     // We skip the actual postprocessing step here, as it is not the focus of this code
